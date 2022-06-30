f347df3ef27faae0b88544af36fec2fb8cde0b16
johnjasa <johnjasa11@gmail.com>
2022-06-30 09:36:20 -0500

    Updated functions

--- manim_helper_functions/utils.py
+++ manim_helper_functions/utils.py
@@ -16,8 +16,14 @@ def lagged_write(scene, text_list, delay=1, beginning_text="", final_text=""):
 
         scene.wait(delay)
 
+def write_caption(scene, message, delay=1):
+    caption = Tex("\\raggedright{" + message + "}").scale(0.8)
+    scene.play(Create(caption))
+    scene.wait(delay)
+    scene.play(FadeOut(caption))
 
-def make_title_slide(scene, title, contents_list, main_message):
+
+def make_title_slide(scene, title, contents_list, intro_message, outro_message):
 
     main_title = Tex(title)
     main_title.shift(0.5*UP)
@@ -28,11 +34,7 @@ def make_title_slide(scene, title, contents_list, main_message):
 
     scene.play(Transform(main_title, title_short))
 
-    real_main_message = "\\raggedright{\larger{Main takeaway:} \\\\" + main_message + "}"
-    message_title = Tex(real_main_message).scale(0.8)
-    scene.play(FadeIn(message_title))
-    scene.wait(0.5)
-    scene.play(FadeOut(message_title))
+    write_caption(scene, intro_message)
 
     blist = BulletedList(*contents_list)
 
@@ -48,10 +50,16 @@ def make_title_slide(scene, title, contents_list, main_message):
         scene.wait(0.5)
         scene.play(FadeOut(blist), FadeOut(title_short))
 
+    real_main_message = "\\raggedright{Main takeaway \\newline \\\\ {\large " + outro_message + "}}"
+    message_title = Tex(real_main_message).scale(0.8)
+    scene.play(FadeIn(message_title))
+    scene.wait(0.5)
+    scene.play(FadeOut(message_title))
+
 
 def make_venn(scene, types=['opt', 'mda', 'diff'], show_center_words=False):
     scene.camera.background_color="#2d3c54"
-    font_size = 24
+    font_size = 32
     scene.camera.frame.scale(0.6)
     scene.camera.frame.save_state()
 
@@ -68,41 +76,61 @@ def make_venn(scene, types=['opt', 'mda', 'diff'], show_center_words=False):
     all = VGroup(opt_mda, diff).arrange(DOWN, buff=-1.)
 
     # TODO: tweak alignment of labels
-    Text.set_default(font_size=font_size, font="Noto sans")
+    Tex.set_default(font_size=font_size)
 
-    opt_text = Text("Optimization").set_color(opt_color)
+    opt_text = Tex("Optimization").set_color(opt_color)
     opt_text.next_to(opt.get_corner(UP+LEFT),0.4*UP)
     opt_text.align_to(opt_mda.get_edge_center(UP), RIGHT)
     opt_group = VGroup(opt, opt_text)
 
-    mda_text = Text("Modeling").set_color(mda_color)
+    mda_text = Tex("Modeling").set_color(mda_color)
     mda_text.align_to(opt_text, UP)
     mda_text.align_to(mda.get_edge_center(UP), LEFT)
     mda_group = VGroup(mda, mda_text)
 
-    diff_text = Text("Differentiation").set_color(diff_color)
+    diff_text = Tex("Differentiation").set_color(diff_color)
     diff_text.next_to(diff.get_edge_center(DOWN), 0.1*DOWN)
     diff_group = VGroup(diff, diff_text)
     
-    scene.play(FadeIn(opt), FadeIn(opt_text))
-    scene.play(FadeIn(mda), FadeIn(mda_text))
-    scene.play(FadeIn(diff), FadeIn(diff_text))
-
-    scene.play(scene.camera.frame.animate.scale(0.55).move_to(opt_group))
-    scene.wait(0.5)
-    
-    scene.play(scene.camera.frame.animate.move_to(mda_group))
-    scene.wait(0.5)
-    
-    scene.play(scene.camera.frame.animate.move_to(diff_group))
-    scene.wait(0.5)
-    
-    scene.play(Restore(scene.camera.frame))
-    scene.wait(1)
-
     if show_center_words:
-        mdo_text = Text("MDAO").set_color(BLACK).move_to(0.1*UP)
+        scene.play(FadeIn(opt), FadeIn(opt_text))
+        scene.play(FadeIn(mda), FadeIn(mda_text))
+        scene.play(FadeIn(diff), FadeIn(diff_text))
+
+        scene.play(scene.camera.frame.animate.scale(0.55).move_to(opt_group))
+        scene.wait(0.5)
+        
+        scene.play(scene.camera.frame.animate.move_to(mda_group))
+        scene.wait(0.5)
+        
+        scene.play(scene.camera.frame.animate.move_to(diff_group))
+        scene.wait(0.5)
+        
+        scene.play(Restore(scene.camera.frame))
+        scene.wait(1)
+        mdo_text = Tex("MDAO").set_color(BLACK).move_to(0.1*UP)
 
         scene.play(FadeIn(mdo_text))
 
         scene.wait(2)
+
+    else:
+        scene.play(FadeIn(opt, opt_text, mda, mda_text, diff, diff_text))
+        scene.wait(1.5)
+
+        list_to_fade_out = []
+        if "opt" not in types:
+            list_to_fade_out.extend([opt, opt_text])
+        if "mda" not in types:
+            list_to_fade_out.extend([mda, mda_text])
+        if "diff" not in types:
+            list_to_fade_out.extend([diff, diff_text])
+
+        scene.play(FadeOut(*list_to_fade_out))
+        scene.wait(2)
+
+    scene.play(
+        *[FadeOut(mob)for mob in scene.mobjects]
+    )
+
+
