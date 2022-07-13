1ddc6a7f98ad8fa8234d1a18f4d68a1775e57e11
johnjasa <johnjasa11@gmail.com>
2022-07-13 16:59:13 -0500

    Updates throughout for basic opt problem

--- Optimization/basic_opt_formulation.py
+++ Optimization/basic_opt_formulation.py
@@ -1,9 +1,6 @@
 from manim import *
 import numpy as np
 from manim_helper_functions import *
-import matplotlib as mpl
-import matplotlib.pyplot as plt
-from PIL import Image
 import openmdao.api as om
 
 
@@ -152,7 +149,7 @@ class SequenceForExample(Scene):
         self.play(*[FadeOut(mob)for mob in self.mobjects])
 
 
-class SampleOptimization(Scene):
+class SampleOptimization(MovingCameraScene):
     def construct(self):
         self.camera.background_color="#2d3c54"
 
@@ -160,48 +157,11 @@ class SampleOptimization(Scene):
         def objective(x, y):
             return (x**2 + y - 11)**2 + (x + y**2 -7)**2
         
-        # define range for input
-        r_min, r_max = -4., 4.
-        # sample input range uniformly at 0.1 increments
-        xaxis = np.arange(r_min, r_max, 0.01)
-        yaxis = np.arange(r_min, r_max, 0.01)
-        # create a mesh from the axis
-        x, y = np.meshgrid(xaxis, yaxis)
-        # compute targets
-        results = objective(x, y)
-
-        cm_hot = mpl.cm.get_cmap('viridis')
-        im = np.array(results / np.max(results))
-        im = cm_hot(im)
-        im = np.uint8(im * 255)
-
-        plt.figure(figsize=(10, 8))
-        contours = plt.contour(x, y, results, 15, colors='black')
-        plt.clabel(contours, inline=True, fontsize=12)
-        plt.imshow(results, extent=[r_min, r_max, r_min, r_max], origin='lower', cmap='RdGy', alpha=0.5)
-        ax = plt.gca()
-        ax.axis('off')
-        plt.savefig('out.png', transparent=True, bbox_inches="tight", dpi=300)
-
-        im = Image.open("out.png")
-        im2 = im.crop(im.getbbox())
-        im2.save("out.png")
-
-        image = ImageMobject('out.png').scale(0.55)
-
-        ax = Axes(
-            x_range=(r_min, r_max, 1),
-            y_range=(r_min, r_max, 1),
-            x_length=r_max - r_min,
-            y_length=r_max - r_min,
-            tips=False,
-            axis_config={"include_numbers": True, "numbers_to_exclude": [r_min, r_max]},
-        ).scale_to_fit_height(image.height)
+        image, ax = plot_2d_function(objective)
 
         self.play(FadeIn(image, ax))
 
         self.wait()
-
 
         # Plot the minima and maxima
         minima = [
@@ -213,22 +173,8 @@ class SampleOptimization(Scene):
 
         maxima = [[-.270845, -.923039]]
 
-        def plot_dots(points):
-            dots = VGroup()
-            for idx, point in enumerate(points):
-                dot = Dot(color=WHITE, stroke_width=.02, radius=0.06, stroke_color=BLACK).move_to(ax.c2p(point[0], point[1]))
-                dots.add(dot)
-            self.play(FadeIn(dots))
-            anims = []
-            for dot in dots:
-                anims.append(Flash(dot, color=WHITE))
-            self.play(LaggedStart(*anims, lag_ratio=0.25))
-            self.wait()
-            self.play(FadeOut(dots))
-        
-        plot_dots(maxima)
-        plot_dots(minima)
-
+        plot_dots(self, maxima, ax)
+        plot_dots(self, minima, ax)
 
 
         excomp = om.ExecComp('obj=(x**2 + y - 11)**2 + (x + y**2 -7)**2')
@@ -254,61 +200,10 @@ class SampleOptimization(Scene):
 
         # run the optimization
         prob.run_driver();
-
-        def get_results(filename):
-            cr = om.CaseReader(filename)
-            cases = cr.get_cases()
-
-            results = {}
-            for case in cases:
-                for key in case.outputs.keys():
-                    if key not in results.keys():
-                        results[key] = []
-                    results[key].append(case.outputs[key])
-
-            for key in case.outputs.keys():
-                results[key] = np.array(results[key])
-
-            return results
-
         results = get_results('unconstrained.sql')
-
-
-
-        x_point = ValueTracker(1.0)
-        y_point = ValueTracker(1.0)
-
-        def draw_tangent_line_on_curve(x_value, y_value):
-            dot = always_redraw(
-                lambda: Dot(color=WHITE, stroke_width=.02, radius=0.06, stroke_color=BLACK).move_to(
-                    ax.c2p(x_value.get_value(), y_value.get_value()))
-                )
-            return dot
-
-        dot = draw_tangent_line_on_curve(x_point, y_point)
-
-        def draw_results(results):
-            x_prev = 1
-            y_prev = 1
-            self.play(FadeIn(dot))
-            lines = VGroup()
-            for idx, (x, y) in enumerate(zip(results['x'], results['y'])):
-                line = Line(ax.c2p(x_prev, y_prev), ax.c2p(x, y))
-                line.set_stroke(opacity=0.5)
-                self.play(Create(line), x_point.animate.set_value(x), y_point.animate.set_value(y), run_time=.5, rate_func=linear)
-                self.wait(0.1)
-                lines.add(line)
-                x_prev = x
-                y_prev = y
-            self.wait()
-            
-            self.play(FadeOut(dot), FadeOut(lines))
-
-        draw_results(results)
+        draw_results(self, results, ax)
 
         
-
-
         excomp = om.ExecComp('obj=(x**2 + y - 11)**2 + (x + y**2 -7)**2')
         constraint_comp = om.ExecComp('con=x**2 + y**2')
 
@@ -346,16 +241,7 @@ class SampleOptimization(Scene):
         self.play(FadeIn(circle), FadeIn(un))
         self.wait()
         
-        x_point.set_value(1.0)
-        y_point.set_value(1.0)
-
-        dot = draw_tangent_line_on_curve(x_point, y_point)
-        
-        draw_results(results)
-
-
-
-        
+        draw_results(self, results, ax)
 
 
         excomp = om.ExecComp('obj=(x**2 + y - 11)**2 + (x + y**2 -7)**2')
@@ -395,13 +281,114 @@ class SampleOptimization(Scene):
         self.play(Create(line))
         self.wait()
         
-        x_point.set_value(1.0)
-        y_point.set_value(1.0)
+        draw_results(self, results, ax)
+        
+        clear(self)
 
-        dot = draw_tangent_line_on_curve(x_point, y_point)
+
+class PoorlyPosed(MovingCameraScene):
+    def construct(self):
+        self.camera.background_color="#2d3c54"
+
+        # objective function
+        def objective(x, y):
+            return (x**2 + y**2 - 1)**2.
         
-        draw_results(results)
+        image, ax = plot_2d_function(objective, r_min=-2, r_max=2, font_size=18)
+        self.play(FadeIn(image, ax))
+        self.wait()
+
+        unit_conversion = np.linalg.norm(ax.c2p(1., 0.))
+        radius = unit_conversion
+        circle = Circle(radius, color=WHITE)
+        self.play(Create(circle))
+        self.play(Flash(
+            circle, line_length=1,
+            num_lines=30, color=WHITE,
+            flash_radius=radius+SMALL_BUFF,
+            time_width=0.3, run_time=1,
+            rate_func = rush_from
+        ))
+        self.wait()
+        self.play(circle.animate.set_stroke(opacity=0.3))
+
+
+        excomp = om.ExecComp('obj=(x**2 + y**2 - 1)**2.')
+
+        prob = om.Problem()
+
+        prob.model.add_subsystem('excomp', excomp, promotes=['*'])
+
+        # setup the optimization
+        prob.driver = om.ScipyOptimizeDriver()
+        prob.driver.options['optimizer'] = 'SLSQP'
+        prob.driver.options['tol'] = 1.e-9
+
+        prob.driver.recording_options['includes'] = ['*']
+        recorder = om.SqliteRecorder("poorly_posed.sql")
+        prob.driver.add_recorder(recorder)
+
+        prob.model.add_design_var('x', lower=-4., upper=4.)
+        prob.model.add_design_var('y', lower=-4., upper=4.)
+        prob.model.add_objective('obj')
+
+        prob.setup()
+
+        prob.set_val('x', 0.5)
+        prob.set_val('y', 1.5)
+
+        # run the optimization
+        prob.run_driver();
+        results = get_results('poorly_posed.sql')
+        draw_results(self, results, ax)
+
+        prob.set_val('x', -0.25)
+        prob.set_val('y', -.5)
+
+        # run the optimization
+        prob.run_driver();
+        results = get_results('poorly_posed.sql')
+        draw_results(self, results, ax)
         
-        self.play(
-            *[FadeOut(mob)for mob in self.mobjects]
-        )
+        clear(self)
+
+class WellPosed(MovingCameraScene):
+    def construct(self):
+        self.camera.background_color="#2d3c54"
+
+        # objective function
+        def objective(x, y):
+            return ((x-0.5)**2 + 0.5*(y-1.0)**2 + x*y - 0.5)
+        
+        image, ax = plot_2d_function(objective, r_min=-2, r_max=2, font_size=18)
+        self.play(FadeIn(image, ax))
+        self.wait()
+
+        excomp = om.ExecComp('obj=(x-0.5)**2 + 0.5*(y-1.0)**2 + x*y - 0.5')
+
+        prob = om.Problem()
+
+        prob.model.add_subsystem('excomp', excomp, promotes=['*'])
+
+        # setup the optimization
+        prob.driver = om.ScipyOptimizeDriver()
+        prob.driver.options['optimizer'] = 'SLSQP'
+        prob.driver.options['tol'] = 1.e-9
+
+        prob.driver.recording_options['includes'] = ['*']
+        recorder = om.SqliteRecorder("wellposed.sql")
+        prob.driver.add_recorder(recorder)
+
+        prob.model.add_design_var('x', lower=-4., upper=4.)
+        prob.model.add_design_var('y', lower=-4., upper=4.)
+        prob.model.add_objective('obj')
+
+        prob.setup()
+
+        prob.set_val('x', 0.5)
+        prob.set_val('y', 1.5)
+
+        # run the optimization
+        prob.run_driver();
+        results = get_results('wellposed.sql')
+        draw_results(self, results, ax)
--- manim_helper_functions/utils.py
+++ manim_helper_functions/utils.py
@@ -1,10 +1,13 @@
 from manim import *
 from manim_pptx import *
 import numpy as np
+import matplotlib.pyplot as plt
+from PIL import Image
+import openmdao.api as om
+from pyxdsm.XDSM import XDSM, OPT, SOLVER, FUNC, LEFT
 
 
 def lagged_write(scene, text_list, delay=1, beginning_text="", final_text="", fresh_between=False):
-
     prev_tex_table = Tex(beginning_text + text_list[0] + final_text)
     scene.play(Write(prev_tex_table))
     scene.wait(delay)
@@ -101,7 +104,6 @@ def make_venn(scene, types=['opt', 'mda', 'diff'], show_center_words=False):
 
 
 def make_title_slide(scene, title, contents_list, intro_message, outro_message, venn_types=["opt"]):
-
     main_title = Tex(title)
     main_title.shift(0.5*UP)
 
@@ -136,3 +138,142 @@ def make_title_slide(scene, title, contents_list, intro_message, outro_message, 
     scene.play(FadeIn(message_title))
     scene.wait(0.5)
     scene.play(FadeOut(message_title))
+
+
+def plot_2d_function(function_to_plot, r_min=-4., r_max=4., font_size=24):
+    # objective function
+    def objective(x, y):
+        return (x**2 + y - 11)**2 + (x + y**2 -7)**2
+    
+    # sample input range uniformly at 0.1 increments
+    xaxis = np.arange(r_min, r_max, 0.01)
+    yaxis = np.arange(r_min, r_max, 0.01)
+    # create a mesh from the axis
+    x, y = np.meshgrid(xaxis, yaxis)
+    # compute targets
+    results = function_to_plot(x, y)
+
+    plt.figure(figsize=(10, 8))
+    contours = plt.contour(x, y, results, 15, colors='black')
+    plt.clabel(contours, inline=True, fontsize=12)
+    plt.imshow(results, extent=[r_min, r_max, r_min, r_max], origin='lower', cmap='RdGy', alpha=0.5)
+    ax = plt.gca()
+    ax.axis('off')
+    plt.savefig('out.png', transparent=True, bbox_inches="tight", dpi=300)
+
+    im = Image.open("out.png")
+    im2 = im.crop(im.getbbox())
+    im2.save("out.png")
+
+    image = ImageMobject('out.png').scale(0.55)
+
+    ax = Axes(
+        x_range=(r_min, r_max, 1),
+        y_range=(r_min, r_max, 1),
+        x_length=r_max - r_min,
+        y_length=r_max - r_min,
+        tips=False,
+        axis_config={"include_numbers": True, "numbers_to_exclude": [r_min, r_max], "font_size": font_size},
+    ).scale_to_fit_height(image.height)
+
+    return image, ax
+
+def get_results(filename):
+    cr = om.CaseReader(filename)
+    cases = cr.get_cases()
+
+    results = {}
+    for case in cases:
+        for key in case.outputs.keys():
+            if key not in results.keys():
+                results[key] = []
+            results[key].append(case.outputs[key])
+
+    for key in case.outputs.keys():
+        results[key] = np.array(results[key])
+
+    return results
+
+def plot_dots(scene, points, ax):
+    dots = VGroup()
+    for idx, point in enumerate(points):
+        dot = Dot(color=WHITE, stroke_width=.02, radius=0.06, stroke_color=BLACK).move_to(ax.c2p(point[0], point[1]))
+        dots.add(dot)
+    scene.play(FadeIn(dots))
+    anims = []
+    for dot in dots:
+        anims.append(Flash(dot, color=WHITE))
+    scene.play(LaggedStart(*anims, lag_ratio=0.25))
+    scene.wait()
+    scene.play(FadeOut(dots))
+
+
+def plot_point_animation(x_value, y_value, ax):
+    dot = always_redraw(
+        lambda: Dot(color=WHITE, stroke_width=.02, radius=0.06, stroke_color=BLACK).move_to(
+            ax.c2p(x_value.get_value(), y_value.get_value()))
+        )
+    return dot
+
+
+def draw_results(scene, results, ax):
+    x_point = ValueTracker(results['x'][0])
+    y_point = ValueTracker(results['y'][0])
+    x_prev = x_point.get_value()
+    y_prev = y_point.get_value()
+    dot = plot_point_animation(x_point, y_point, ax)
+    scene.play(FadeIn(dot))
+    lines = VGroup()
+    for idx, (x, y) in enumerate(zip(results['x'], results['y'])):
+        line = Line(ax.c2p(x_prev, y_prev), ax.c2p(x, y))
+        line.set_stroke(opacity=0.5)
+        scene.play(Create(line), x_point.animate.set_value(x), y_point.animate.set_value(y), run_time=.5, rate_func=linear)
+        scene.wait(0.1)
+        lines.add(line)
+        x_prev = x
+        y_prev = y
+    scene.wait()
+    
+    scene.play(FadeOut(dot), FadeOut(lines))
+
+def clear(scene):
+    scene.play(
+        *[FadeOut(mob)for mob in scene.mobjects]
+    )
+
+def add_all(scene):
+    scene.play(
+        *[FadeIn(mob)for mob in scene.mobjects]
+    )
+
+def make_xdsm(scene):
+
+    # Change `use_sfmath` to False to use computer modern
+    x = XDSM(use_sfmath=True)
+
+    x.add_system("opt", OPT, r"\text{Optimizer}")
+    x.add_system("solver", SOLVER, r"\text{Newton}")
+    x.add_system("D1", FUNC, "D_1")
+    x.add_system("D2", FUNC, "D_2")
+    x.add_system("F", FUNC, "F")
+    x.add_system("G", FUNC, "G")
+
+    x.connect("opt", "D1", "x, z")
+    x.connect("opt", "D2", "z")
+    x.connect("opt", "F", "x, z")
+    x.connect("solver", "D1", "y_2")
+    x.connect("solver", "D2", "y_1")
+    x.connect("D1", "solver", r"\mathcal{R}(y_1)")
+    x.connect("solver", "F", "y_1, y_2")
+    x.connect("D2", "solver", r"\mathcal{R}(y_2)")
+    x.connect("solver", "G", "y_1, y_2")
+
+    x.connect("F", "opt", "f")
+    x.connect("G", "opt", "g")
+
+    x.add_output("opt", "x^*, z^*", side=LEFT)
+    x.add_output("D1", "y_1^*", side=LEFT)
+    x.add_output("D2", "y_2^*", side=LEFT)
+    x.add_output("F", "f^*", side=LEFT)
+    x.add_output("G", "g^*", side=LEFT)
+    x.write("mdf")
